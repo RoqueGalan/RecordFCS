b80b111b3551053432626a824f9e29fc99e89cb5
Yuli <yr.sistemas@soumaya.org.mx>
2015-08-20 09:58:11 -0500

    cambios css

---
+++ RecordFCS/RecordFCS/Models/Historial/log_ObjetoAtributosAdicional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace RecordFCS.Models.Historial
+{
+    public class log_ObjetoAtributosAdicional
+    {
+        [Key]
+        public Guid log_ObjetoAtributosAdicionalID { get; set; }
+
+        [Required]
+        public Guid log_ObjetoID { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string AtributoNombre { get; set; }
+
+        public string AtributoValor { get; set; }
+
+        [ForeignKey("log_ObjetoID")]
+        public virtual log_Objeto log_Objeto { get; set; }
+    }
+}
